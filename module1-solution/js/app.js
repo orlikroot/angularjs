727a9177efe2d136cfd2f2d779418a7f8d6e3c49
Евгений Кротов <orlikroot@gmail.com>
2016-09-15 16:36:37 +0300

    Change controller name

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('lunchCheckerApp', [])
-        .controller('MsgController', MsgController);
+        .controller('LunchCheckController', LunchCheckController);
 
 
 })();
