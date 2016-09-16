11ae0c6ded817d131159f62976b6660828a3e316
Евгений Кротов <orlikroot@gmail.com>
2016-09-16 13:48:52 +0300

    Test OK

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -1,8 +1,26 @@
 (function () {
     'use strict';
 
-    angular.module('lunchCheckerApp', [])
+    angular.module('LunchCheckerApp', [])
         .controller('LunchCheckController', LunchCheckController);
 
-
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+    $scope.click = function () {
+        var inputString;
+        var outputString;
+        var arrayOfStrings;
+        if ($scope.input) {
+            inputString = $scope.input;
+            arrayOfStrings = inputString.split(',');
+            if (arrayOfStrings.length <= 3) {
+                outputString = "Enjoy!"
+            }
+            if (arrayOfStrings.length > 3) {
+                outputString = "Too much!"
+            }
+        } else {outputString = "Please enter data first";}
+        $scope.output = outputString;
+    }
+    }
 })();
